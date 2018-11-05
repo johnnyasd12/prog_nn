a2bff65e7555508ffa925eedd8c3e5b33e7c583a
johnnyasd12 <johnnnyasd12@gmail.com>
2018-11-05 19:41:28 +0800

    support softmax, tf.float64, validation_set

--- progressive_nn.py
+++ progressive_nn.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 from pprint import pprint
 from param_collection import ParamCollection
+from tensorflow.examples.tutorials.mnist import input_data
 
 class InitialColumnProgNN(object):
 
@@ -46,16 +47,16 @@ class InitialColumnProgNN(object):
         
     def weight_fc(self, shape, stddev=0.1, initial=None):
         if initial is None:
-#             initial = tf.truncated_normal(shape,stddev=stddev,dtype=self.dtype_X)
-#             initial = tf.Variable(initial)
-            initial = tf.Variable(tf.random_normal(shape))
+            initial = tf.truncated_normal(shape,stddev=stddev,dtype=self.dtype_X)
+            initial = tf.Variable(initial)
+            # initial = tf.Variable(tf.random_normal(shape), dtype=self.dtype_X)
         return initial
 
     def bias_fc(self, shape, init_bias=0.1, initial=None):
         if initial is None:
-#             initial = tf.constant(init_bias,shape=shape,dtype=self.dtype_X)
-#             initial = tf.Variable(initial)
-            initial = tf.Variable(tf.zeros(shape) + 0.1)
+            initial = tf.constant(init_bias,shape=shape,dtype=self.dtype_X)
+            initial = tf.Variable(initial)
+            # initial = tf.Variable(tf.zeros(shape) + 0.1, dtype=self.dtype_X)
         return initial
     
     def add_fc(
@@ -81,6 +82,7 @@ class InitialColumnProgNN(object):
             out = WX_b
         else:
             out = activation_func(WX_b)
+
         self.topology.append(out_size)
         self.activations.append(activation_func)
         self.L = self.L + 1
@@ -111,7 +113,8 @@ class InitialColumnProgNN(object):
         self.pc = ParamCollection(self.session, self.params)
         
     def train(self, X, y, n_epochs, batch_size=None, data_valid=None, display_steps=50): 
-        # data_valid:tuple
+        # data_valid:list
+
         self.session.run(tf.global_variables_initializer())
         
         n_samples = X.shape[0]
@@ -135,7 +138,15 @@ class InitialColumnProgNN(object):
                 if counter%display_steps==0 or (epoch==n_epochs and step==steps_per_epoch-1):
                     loss_train = self.session.run(self.loss,feed_dict={self.Xs:X_batch, self.ys:y_batch})
                     self.loss_hist_train.append(loss_train)
-                    print('Epoch',epoch,', step',step,', train loss =',loss_train)
+                    print('Epoch',epoch,', step',step,', loss =',loss_train, end=' ')
+                    if data_valid is not None:
+                        X_val = data_valid[0]
+                        y_val = data_valid[1]
+                        loss_val = self.session.run(self.loss,feed_dict={self.Xs:X_val,self.ys:y_val})
+                        self.loss_hist_val.append(loss_val)
+                        print(', val_loss =',loss_val)
+                    else:
+                        print()
                 counter += 1
                 
     
@@ -143,7 +154,24 @@ class InitialColumnProgNN(object):
         return self.session.run(self.prediction,feed_dict={self.Xs:X})
 
 
-
+def check_obj(obj_str):
+    obj = eval(obj_str)
+    obj_type = type(obj)
+    print(obj_str
+        , obj_type
+        , end = ' '
+        )
+    if obj_type == np.ndarray:
+        print(obj.shape)
+    else:
+        try:
+            iterator = iter(obj)
+        except TypeError:
+            # not iterable
+            print(obj)
+        else:
+            # iterable
+            print(len(obj))
 
 if __name__ == "__main__":
     mem_fraction = 0.25
@@ -158,36 +186,74 @@ if __name__ == "__main__":
 #     X_data = np.random.random((6000))[:, np.newaxis]*100
 #     noise = np.random.normal(0, 0.05, X_data.shape).astype(np.float32)*0
 #     y_data = X_data*2 + 1 + noise
-    X_data = np.linspace(-1,1,300, dtype=np.float32)[:, np.newaxis]
-    noise = np.random.normal(0, 0.05, X_data.shape).astype(np.float32)
-    y_data = np.square(X_data) - 0.5 + noise
-    print('X_data',X_data.shape,'\n',X_data[:5])
-    print('y_data',y_data.shape,'\n',y_data[:5])
-    
-    input_dims = X_data.shape[1]
-    col_0 = InitialColumnProgNN(
-        input_dims=input_dims
-        , output_dims=1
-        , session=session
-        , dtype_X=tf.float32, dtype_y=tf.float32
-    )
-    col_0.add_fc(10,activation_func=tf.nn.relu)
-#     col_0.add_fc(1024,activation_func=tf.nn.relu)
-    col_0.add_fc(1,activation_func=None,output_layer=True)
-    col_0.compile_nn(
-#         loss=tf.reduce_mean(tf.reduce_sum(tf.square(col_0.ys - col_0.prediction),reduction_indices=[1]))
-        loss=tf.losses.mean_squared_error(col_0.ys,col_0.prediction)
-#         ,opt=tf.train.AdamOptimizer(learning_rate=1e-3)
-        ,opt=tf.train.GradientDescentOptimizer(learning_rate=1e-1)
-#         ,mectrics=[]
-    )
-    col_0.train(
-        X=X_data
-        , y=y_data
-        , batch_size=None
-        , n_epochs=1000
-        , display_steps=50
-    )
+    try_reg = False
+    if try_reg:
+        X_data = np.linspace(-1,1,300, dtype=np.float32)[:, np.newaxis]
+        noise = np.random.normal(0, 0.05, X_data.shape).astype(np.float32)
+        y_data = np.square(X_data) - 0.5 + noise
+        print('X_data',X_data.shape,'\n',X_data[:5])
+        print('y_data',y_data.shape,'\n',y_data[:5])
+        
+        input_dims = X_data.shape[1]
+        col_0 = InitialColumnProgNN(
+            input_dims=input_dims
+            , output_dims=1
+            , session=session
+            , dtype_X=tf.float32, dtype_y=tf.float32
+        )
+        col_0.add_fc(10,activation_func=tf.nn.relu)
+    #     col_0.add_fc(1024,activation_func=tf.nn.relu)
+        col_0.add_fc(1,activation_func=None,output_layer=True)
+        col_0.compile_nn(
+    #         loss=tf.reduce_mean(tf.reduce_sum(tf.square(col_0.ys - col_0.prediction),reduction_indices=[1]))
+            loss=tf.losses.mean_squared_error(col_0.ys,col_0.prediction)
+    #         ,opt=tf.train.AdamOptimizer(learning_rate=1e-3)
+            ,opt=tf.train.GradientDescentOptimizer(learning_rate=1e-1)
+    #         ,mectrics=[]
+        )
+        col_0.train(
+            X=X_data
+            , y=y_data
+            , batch_size=None
+            , n_epochs=1000
+            , display_steps=50
+        )
+
+    try_cls = True
+    if try_cls:
+        mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+        X_train = mnist.train.images#.astype(np.float64)
+        y_train = mnist.train.labels#.astype(np.float64)
+        X_val = mnist.validation.images
+        y_val = mnist.validation.labels
+        X_test = mnist.test.images
+        y_test = mnist.test.labels
+        check_obj('X_train')
+        check_obj('y_train')
+        input_dims = X_train.shape[1]
+        output_dims = y_train.shape[1]
+        check_obj('input_dims')
+        check_obj('output_dims')
+        col_cls_0 = InitialColumnProgNN(
+            input_dims=input_dims
+            , output_dims=output_dims
+            , session=session
+            , dtype_X=tf.float64
+            , dtype_y=tf.float64)
+        col_cls_0.add_fc(512,activation_func=tf.nn.relu)
+        col_cls_0.add_fc(256,activation_func=tf.nn.relu)
+        col_cls_0.add_fc(output_dims,activation_func=tf.nn.softmax,output_layer=True)
+        col_cls_0.compile_nn(
+            loss=tf.losses.softmax_cross_entropy(col_cls_0.ys,col_cls_0.logits)
+            , opt=tf.train.AdamOptimizer(learning_rate=1e-3))
+        col_cls_0.train(
+            X=X_train
+            ,y=y_train
+            ,data_valid=[X_val,y_val]
+            ,batch_size=128
+            ,n_epochs=20
+            ,display_steps=200)
+
 
 
 
